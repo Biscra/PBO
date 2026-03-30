9c7e25547effe736927a73b22656b9b6ce496dd1
Ajen M <diajengwulan2000@gmail.com>
2026-03-30 11:01:30 +0700

    JAWA

---
+++ tugas 1/Soal2.java
@@ -0,0 +1,11 @@
+public class Soal2 {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("        BIODATA MAHASISWA        ");
+        System.out.println("=================================");
+        System.out.println("Nama   : Ado");
+        System.out.println("NIM    : 2024004025");
+        System.out.println("Kelas  : Informatika");
+        System.out.println("=================================");
+    }
+}
